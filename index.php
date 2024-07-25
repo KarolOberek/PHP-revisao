19c179474ab6227be672873ddf0ad4e9aaadc162
KarolineOberek <karolineoberek@gmail.com>
2024-07-25 00:19:10 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<head>
+
+</head>
+<body>
+	<?php
+	$string = "Curso DIO"
+	?>
+	<h1>
+		Cursos de variáveis em PHP
+	</h1>
+	<h4>
+		Essa é uma variável do tipo string.
+			Para implementar uma variável do tipo string, utilizamos aspas simples ('') ou aspas duplas ("")
+	</h4>
+	<?php
+	echo $string
+	?>
+</body>
+</html>
