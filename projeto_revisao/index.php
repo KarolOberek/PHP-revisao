bf710580d1736961217a536206c6c343a2d3c17c
KarolineOberek <karolineoberek@gmail.com>
2024-07-25 00:38:57 -0300

    array

--- projeto_revisao/index.php
+++ projeto_revisao/index.php
@@ -5,6 +5,7 @@
 	<?php
 	$string = "Cursos DIO";
 	$numeros = 50;
+	$array = array("PHP", "HTML", "CSS", "JS");
 	?>
 	<h1>
 		Cursos de variáveis em PHP
@@ -17,10 +18,15 @@
 	echo $string;
 	?>
 	<h4>Essa é uma variável do tipo número, ou como também é conhecida, integer.
-		 Para implementaressa variável utilizamos somente os números desta forma:
+		 Para implementar essa variável utilizamos somente os números desta forma:
 	</h4>
 	<?php
 	echo $numeros;
 	?>
+	<h4>Essa é uma variável com um array. Para implementar um array utilizamos
+		 o seguinte formato:  </h4>
+		 <?php
+		print_r($array)
+		 ?>
 </body>
 </html>
